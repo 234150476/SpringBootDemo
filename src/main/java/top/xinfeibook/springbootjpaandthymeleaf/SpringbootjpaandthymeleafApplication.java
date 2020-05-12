3d74d70400128d6fb0430f48e18cef6e3e7e2c64
陈飞阳 <234150476@qq.com>
2020-05-13 01:09:21 +0800

    开发版本2.0测试

--- src/main/java/top/xinfeibook/springbootjpaandthymeleaf/SpringbootjpaandthymeleafApplication.java
+++ src/main/java/top/xinfeibook/springbootjpaandthymeleaf/SpringbootjpaandthymeleafApplication.java
@@ -8,6 +8,7 @@ public class SpringbootjpaandthymeleafApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootjpaandthymeleafApplication.class, args);
         System.out.println("我是dev1.0");
+        System.out.println("我是dev2.0");
     }
 
 }
