d947322bcb86f210374a6ceee3185340e55cd3b4
234150476 <234150476@qq.com>
2020-05-12 17:27:47 +0000

    Update SpringbootjpaandthymeleafApplication.java

--- src/main/java/top/xinfeibook/springbootjpaandthymeleaf/SpringbootjpaandthymeleafApplication.java
+++ src/main/java/top/xinfeibook/springbootjpaandthymeleaf/SpringbootjpaandthymeleafApplication.java
@@ -8,6 +8,7 @@ public class SpringbootjpaandthymeleafApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootjpaandthymeleafApplication.class, args);
         System.out.println("正式版1.0");
+        System.out.println("服务器测试冲突");
     }
 
 }
