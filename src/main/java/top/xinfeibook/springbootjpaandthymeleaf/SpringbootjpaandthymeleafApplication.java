bc2d9e91c083bbc17512e8532ed12db0397197cc
陈飞阳 <234150476@qq.com>
2020-05-13 01:08:03 +0800

    开发版本1.0测试

--- src/main/java/top/xinfeibook/springbootjpaandthymeleaf/SpringbootjpaandthymeleafApplication.java
+++ src/main/java/top/xinfeibook/springbootjpaandthymeleaf/SpringbootjpaandthymeleafApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootjpaandthymeleafApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootjpaandthymeleafApplication.class, args);
+        System.out.println("我是dev1.0");
     }
 
 }
